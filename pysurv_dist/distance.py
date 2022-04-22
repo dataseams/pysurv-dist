b1299d45c62b2ff586a9d477ba062bfcde304bf3
Jacob Dodd <jacobdodd94@gmail.com>
2022-04-22 16:00:23 -0700

    update distance functions

--- pysurv_dist/distance.py
+++ pysurv_dist/distance.py
@@ -3,16 +3,44 @@ from numpy.typing import ArrayLike
 from sksurv.utils import Surv
 
 
+def _survival_distance_score(x: ArrayLike, y: Surv) -> float:
+    score = 0
+    return score
+
+
 def survival_distance_score(X: ArrayLike, y: Surv) -> dict:
     """Calculate survival distance score."""
-    pass
+    scores = {}
+    for col in X.columns:
+        scores[col] = _survival_distance_score(X[col], y)
+    return scores
+
+
+def _clinical_independence_score(x: ArrayLike, y: ArrayLike) -> float:
+    score = 0
+    return score
 
 
 def clinical_indpendence_score(X: ArrayLike, y: Surv, **kwargs) -> dict:
-    """Calculate the clinical indpendence score."""
-    pass
+    """Calculate the clinical indpendence score.
+
+    Train a linear regression model for each column in X against every other
+    column.
+
+    The clinical independence score is the following:
+
+        1 - R**2
+    """
+    scores = {}
+    for col in X.columns:
+        scores[col] = _clinical_independence_score(X[col], y)
+    return scores
 
 
-def combine_sds_ci_scores(sds: dict, ci: dict, weight: float):
+def combine_sds_ci_scores(sds: dict, ci: dict, weight: float) -> dict:
     """Combine clinical independence and survival distance scores."""
-    pass
+    # check the keys are shared among both sds and ci
+
+    # combine
+    scores = {}
+    return scores
