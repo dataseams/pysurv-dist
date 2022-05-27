9b705d397cc20814f5359ff8405a4f438e53fb53
Jacob Dodd <jacobdodd94@gmail.com>
2022-05-27 16:56:50 -0700

    Add clinical independence score
    Co-authored-by: Sam Mourad <sammo@users.noreply.github.com>

--- pysurv_dist/distance.py
+++ pysurv_dist/distance.py
@@ -1,10 +1,25 @@
-"""Implentation of survival distance score and clinical independence score."""
+"""Implementation of survival distance score and clinical independence score.
+
+The survival distance score was defined in this paper:
+
+https://psb.stanford.edu/psb-online/proceedings/psb20/Neums.pdf
+
+The goal of the survival distance score is to measure the variation in survival
+rate across time given a particular feature.
+
+"""
+import numpy as np
 from numpy.typing import ArrayLike
-from sksurv.utils import Surv
+from sksurv.util import Surv, check_y_survival
+from sklearn.linear_model import LinearRegression
 
 
 def _survival_distance_score(x: ArrayLike, y: Surv) -> float:
     score = 0
+    event, time = check_y_survival(y)
+    for t in np.sort(time.unique()):
+        # get mean / variance of feature
+        pass
     return score
 
 
@@ -17,23 +32,55 @@ def survival_distance_score(X: ArrayLike, y: Surv) -> dict:
 
 
 def _clinical_independence_score(x: ArrayLike, y: ArrayLike) -> float:
-    score = 0
+    """Calculate the score for clinical independence.
+
+    Parameters
+    ----------
+    x : ArrayLike
+        All but one attribute
+    y : ArrayLike
+        The attribute values we're fitting the linear regression for
+
+    Returns
+    -------
+    score : float
+        cg = 1 - R^2, R^2 being the coefficient of determination
+    """
+    reg = LinearRegression()
+    reg.fit(x, y)
+    score = 1 - reg.score(x, y)
     return score
 
 
-def clinical_indpendence_score(X: ArrayLike, y: Surv, **kwargs) -> dict:
-    """Calculate the clinical indpendence score.
+def clinical_independence_score(X: ArrayLike) -> ArrayLike:
+    """Calculate the clinical independence score.
 
-    Train a linear regression model for each column in X against every other
-    column.
+    Train a linear regression model for each feature in X against all other
+    features to measure how independent a given feature is compared to the
+    other features provided.
 
-    The clinical independence score is the following:
+    The clinical independence score for one feature is the following:
+        1 - R^2
 
-        1 - R**2
+    The final result is a 1D array with clinical independence value for each
+    feature
+
+    Parameters
+    ----------
+    X: ArrayLike
+        All feature attributes
+    y: ArrayLike
+        Survival values
+
+    Returns
+    -------
+    ArrayLike
+        Clinical independence score for column in order of the features given
     """
-    scores = {}
-    for col in X.columns:
-        scores[col] = _clinical_independence_score(X[col], y)
+    scores = [
+        _clinical_independence_score(x=X.drop(col, axis=1), y=X[col])
+        for col in X.columns
+    ]
     return scores
 
 
---
+++ tests/test_distance.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+import pandas as pd
+
+from pysurv_dist import distance
+
+
+class TestClinicalIndependenceScore(TestCase):
+    def setUp(self) -> None:
+        self.data = pd.DataFrame(
+            {
+                "x1": range(10),
+                "x2": [20, 22, 23.5, 24, 25, 28, 28, 29, 29.5, 30],
+                "x3": range(50, 60),
+            }
+        )
+        self.random_data = pd.DataFrame(
+            {
+                "x1": [1, 5, 10, 250, 55, 36, 1, 36, 42, 78],
+                "x2": [20, 21.1, 21.9, 23, 24, 25, 26, 27, 28, 29],
+            }
+        )
+
+    @patch("pysurv_dist.distance._clinical_independence_score")
+    def test_happy_path(self, mock_cg):
+        expected_scores = [1, 2, 3]
+        mock_cg.side_effect = expected_scores
+        scores = distance.clinical_independence_score(self.data)
+        self.assertEqual(scores, expected_scores)
+
+    def test__clinical_independence_score(self):
+        score = distance._clinical_independence_score(
+            self.data[["x1"]], self.data["x2"]
+        )
+        expected_score = 0.04324611
+        self.assertAlmostEqual(score, expected_score, 8)
+
+    def test__clinical_independence_score_random(self):
+        score = distance._clinical_independence_score(
+            self.random_data[["x1"]], self.random_data["x2"]
+        )
+        expected_score = 0.994
+        self.assertAlmostEqual(score, expected_score, 3)
